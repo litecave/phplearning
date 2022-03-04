7e18beb916675573d653d3ef17a3ec824fba3c55
Daniil <64094645+litecave@users.noreply.github.com>
2022-03-04 18:52:15 +0300

    lol

---
+++ phpplusjs/php and js to json setting and showing/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>
+            folder name
+        </title>
+    </head>
+    <body>
+        <form name="text" action="" method="get">
+            <input type="text" name="subject" id="subject" value="Car Loan">
+        </form>
+    </body>
+</html>
